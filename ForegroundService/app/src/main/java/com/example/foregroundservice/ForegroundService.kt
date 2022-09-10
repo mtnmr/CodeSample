7cbb874438cabd8bcbbef853ba5f9a71fb7f08f5
mtnmr <mei.rila1013@gmail.com>
2022-09-10 12:05:31 +0900

    foregroundService sample

---
+++ ForegroundService/app/src/main/java/com/example/foregroundservice/ForegroundService.kt
@@ -0,0 +1,60 @@
+package com.example.foregroundservice
+
+import android.app.*
+import android.app.Notification.FOREGROUND_SERVICE_IMMEDIATE
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+
+class ForegroundService:Service() {
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        Log.d("service test", "start")
+
+        val pendingIntent: PendingIntent =
+            Intent(this, MainActivity::class.java).let { notificationIntent ->
+                PendingIntent.getActivity(this, 0, notificationIntent,
+                    PendingIntent.FLAG_IMMUTABLE)
+            }
+
+        val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("foreground service")
+            .setContentText("foreground service running")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setForegroundServiceBehavior(FOREGROUND_SERVICE_IMMEDIATE)
+            .build()
+
+        Thread(
+            Runnable {
+                Log.d("service test", "start")
+                (0..5).map {
+                    Thread.sleep(1000)
+
+                }
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    stopForeground(Service.STOP_FOREGROUND_DETACH)
+                }else{
+                    stopForeground(true)
+                }
+
+                Log.d("service test", "finish")
+
+            }).start()
+
+
+        startForeground(1, notification)
+
+        return START_STICKY
+    }
+}
--- ForegroundService/app/src/main/java/com/example/foregroundservice/MainActivity.kt
+++ ForegroundService/app/src/main/java/com/example/foregroundservice/MainActivity.kt
@@ -1,43 +1,96 @@
 package com.example.foregroundservice
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.foregroundservice.ui.theme.ForegroundServiceTheme
+
+const val CHANNEL_ID = "foreground service"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        createNotificationChannel()
+
         setContent {
             ForegroundServiceTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen(onClick = { startIntent() })
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun startIntent(){
+        val intent = Intent(this, ForegroundService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Log.d("click test", "startForegroundService")
+            startForegroundService(intent)
+        }else{
+            Log.d("click test", "startService")
+            startService(intent)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ForegroundServiceTheme {
-        Greeting("Android")
+       MainScreen(onClick = {})
+    }
+}
+
+@Composable
+fun MainScreen(
+    onClick: () -> Unit
+){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = onClick
+        ) {
+            Text(text = "Foreground Service Start!")
+        }
     }
 }
